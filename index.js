415afebba7925549e770988b64ea28d14f0208b0
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-05-19 04:23:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uoakm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+	try{
+		await client.connect();
+		const taskCollection = client.db("db_to_do").collection("task");
+		
+		app.post('/addTask', async(req, res) => {
+			const doc = req.body;
+			const result = await taskCollection.insertOne(doc);
+			res.send(result);
+		})
+		app.get('/task/:email', async(req, res) => {
+			const email = req.params.email;
+			const query = {email: email};
+			const result = await taskCollection.find(query).toArray();
+			res.send(result);
+		})
+
+
+
+	} finally{
+
+	}
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+	res.send('To do server is running');
+})
+app.listen(port, () => {
+	console.log('To do server is running');
+})
